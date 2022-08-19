7fdf8c9d537d48e35d403f48e824622598671577
travisoocl <111564237+travisoocl@users.noreply.github.com>
2022-08-19 17:33:48 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 var name = 'hello';
 // TODO write your code here...
 
+console.log(name.toUpperCase())
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
 
+console.log(sentence.replace(/(^\w{1})|(\s+\w{1})/g, x => x.toUpperCase()));
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+
+console.log(money.substr(1))
